d922df97ff26982dd7352779eb94a94a4900bb20
Jessica Ma <jessicam@eventbrite.com>
2016-02-02 16:31:48 -0800

    test

--- server.py
+++ server.py
@@ -7,4 +7,4 @@ app = Flask(__name__, static_url_path='', static_folder='public')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT",4000)))
+    app.run(port=int(os.environ.get("PORT",3000)))
