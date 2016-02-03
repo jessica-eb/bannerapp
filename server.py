5eeeda11734d610521db5508acea372b45ce8745
Jessica Ma <jessicam@eventbrite.com>
2016-02-02 16:36:42 -0800

    change server

--- server.py
+++ server.py
@@ -6,5 +6,6 @@ from flask import Flask, Response, request
 app = Flask(__name__, static_url_path='', static_folder='public')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
-if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT",3000)))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
