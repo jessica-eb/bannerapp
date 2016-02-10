d8ee6666a1fc11e8d1b316d640be827f5554a850
Jessica Ma <jessicam@eventbrite.com>
2016-02-10 13:51:10 -0800

    V2 Final

--- public/scripts/constant.js
+++ public/scripts/constant.js
@@ -1,13 +1,17 @@
 var LAZYOBJ = [
 	{
-		imgsrc: "/images/banners/banner01.png",
+		imgsrc: "/images/banners/728x90.png",
 		name: "728x90",
-		show: false,
+		show: true,
+		thumbstyle: {
+			width: '728px',
+			height: '90px'
+		},
 		logostyle: {
 			top: '50%',
 			left: '4%',
-			width: '20%',
-			color: 'white',
+			width: '10%',
+			color: 'pink',
 			transform: 'translateY(-50%)',
 		},
 		titlerows: '1',
@@ -17,7 +21,7 @@ var LAZYOBJ = [
 			width: '100%',
 			textAlign: 'center',
 			color: 'white',
-			maxHeight: '',
+			fontSize: '20px',
 			maxWidth: '40%',
 			transform: 'translateY(-50%) translateX(-50%)',
 		},
@@ -25,18 +29,24 @@ var LAZYOBJ = [
 			top: '50%',
 			right: '4%',
 			color: 'white',
+			fontSize: '10px',
+			maxWidth: '15%',
 			transform: 'translateY(-50%)'
 		}
 	},
 	{
-		imgsrc: "/images/banners/banner02.png",
+		imgsrc: "/images/banners/468x60.png",
 		name: "469x60",
-		show: false,
+		show: true,
+		thumbstyle: {
+			width: '468px',
+			height: '60px'
+		},
 		logostyle: {
 			top: '50%',
 			left: '4%',
 			width: '18%',
-			color: 'oj-white',
+			color: 'white',
 			transform: 'translateY(-50%)',
 		},
 		titlerows: '1',
@@ -46,6 +56,7 @@ var LAZYOBJ = [
 			width: '100%',
 			textAlign: 'center',
 			color: 'white',
+			fontSize: '18px',
 			maxHeight: '',
 			maxWidth: '40%',
 			transform: 'translateY(-50%) translateX(-50%)',
@@ -54,12 +65,18 @@ var LAZYOBJ = [
 			top: '50%',
 			transform: 'translateY(-50%)',
 			right: '4%',
+			maxWidth: '15%',
+			fontSize:'6px',
 			backgroundColor: '#00cc52'
 		}
 	},
 	{
-		imgsrc: "/images/banners/banner03.png",
+		imgsrc: "/images/banners/300x250.png",
 		name: "300x250",
+		thumbstyle: {
+			width: '300px',
+			height: '250px'
+		},
 		show: true,
 		logostyle: {
 			top: '10%',
@@ -73,144 +90,173 @@ var LAZYOBJ = [
 			left: '10%',
 			width: '100%',
 			color: 'white',
-			maxWidth: '30%'
+			maxWidth: '30%',
+			fontSize: '24px'
 		},
 		bodystyle: {
 			bottom: '0',
 			left: '10%',
+			maxWidth: '40%',
 			backgroundColor: '#00cc52'
 		}
 	},
 	{
-		imgsrc: "/images/banners/banner04.png",
+		imgsrc: "/images/banners/300x600.png",
 		name: "180x150",
+		thumbstyle: {
+			width: '180px',
+			height: '150px'
+		},
 		show: true,
 		logostyle: {
 			top: '10%',
 			left: '10%',
-			width: '80%',
+			width: '30%',
 			color: 'white'
 		},
 		titlerows:'5',
 		titlestyle: {
-			top: '20%',
+			top: '30%',
 			left: '10%',
-			width: '90%',
+			maxWidth: '80%',
 			color: 'white'
 		},
 		bodystyle: {
 			bottom: '10%',
-			left: '50%',
-			transform: 'translateX(-50%)',
-			maxWidth: '100%',
-			backgroundColor: '#00cc52'
+			right: '0',
+			fontSize: '6px',
+			maxWidth: '30%',
+			backgroundColor: '#00cc52',
 		}
 	},
 	{
-		imgsrc: "/images/banners/banner05.png",
+		imgsrc: "/images/banners/300x600.png",
 		name: "300x600",
+		thumbstyle: {
+			width: '300px',
+			height: '600px'
+		},
 		show: true,
 		logostyle: {
-			top: '50%',
-			left: '4%',
-			width: '18%',
+			top: '10%',
+			left: '10%',
+			width: '40%',
 			color: 'white',
-			transform: 'translateY(-50%)',
 		},
-		titlerows: '1',
+		titlerows: '4',
 		titlestyle: {
-			top: '50%',
-			left: '50%',
+			top: '30%',
+			left: '10%',
 			width: '100%',
-			textAlign: 'center',
 			color: 'white',
 			maxHeight: '',
-			maxWidth: '40%',
-			transform: 'translateY(-50%) translateX(-50%)',
+			maxWidth: '60%',
+			fontSize: '28px'
 		},
 		bodystyle: {
-			top: '50%',
-			transform: 'translateY(-50%)',
+			bottom: '30%',
 			right: '0',
 			backgroundColor: 'white'
 		}
 	},
 	{
-		imgsrc: "/images/banners/banner06.png",
+		imgsrc: "/images/banners/160x600.png",
 		name: "160x600",
+		thumbstyle: {
+			width: '160px',
+			height: '600px'
+		},
 		show: true,
 		logostyle: {
 			top: '10%',
 			left: '10%',
 			color: 'white',
-			width: '30%',
+			width: '60%',
 		},
-		titlerows: '3',
+		titlerows: '4',
 		titlestyle: {
 			top: '30%',
 			left: '10%',
 			width: '60%',
 			color: 'white',
-			maxWidth: '30%'
+			maxWidth: '80%',
+			fontSize: '28px',
+			lineHeight: '1.5em'
 		},
 		bodystyle: {
 			bottom: '0',
 			left: '10%',
-			backgroundColor: 'white'
+			backgroundColor: 'white',
+			maxWidth: '50%'
 		}
 	},
 	{
-		imgsrc: "/images/banners/banner07.png",
+		imgsrc: "/images/banners/600x315.png",
 		name: "Facebook",
+		thumbstyle: {
+			width: '600px',
+			height: '314px'
+		},
 		show: true,
 		logostyle: {
 			top: '10%',
-			left: '10%',
-			width: '45%',
+			left: '8%',
+			width: '30%',
 			color: 'white'
 		},
 		titlerows:'3',
 		titlestyle: {
-			top: '30%',
-			left: '10%',
-			width: '80%',
-			color: 'white'
+			top: '40%',
+			left: '8%',
+			maxWidth: '70%',
+			color: 'white',
+			fontSize: '28px'
 		},
 		bodystyle: {
-			maxWidth: '40%',
+			maxWidth: '60%',
 			bottom: '10%',
-			left: '10%',
-			backgroundColor: 'white'
+			left: '8%',
+			maxWidth: '50%',
+			backgroundColor: 'white',
 		}
 	},
 	{
-		imgsrc: "/images/banners/banner08.png",
+		imgsrc: "/images/banners/FB-test.png",
+		name: "FacebookContext",
+		thumbstyle: {
+			width: '719px',
+			height: '362.5px'
+		},
+		show: true,
+		logostyle: {
+			top: '28%',
+			left: '25%',
+			width: '6%',
+			color: 'white'
+		},
+		titlerows:'3',
+		titlestyle: {
+			top: '36%',
+			left: '25%',
+			maxWidth: '25%',
+			color: 'white',
+			fontSize: '8px'
+		},
+		bodystyle: {
+			maxWidth: '25%',
+			top: '48%',
+			left: '25%',
+			backgroundColor: 'white',
+			fontSize: '2px',
+		}
+	},
+	{
+		imgsrc: "/images/banners/800x320.png",
 		name: "Twitter",
-		show: true,
-		logostyle: {
-			top: '10%',
-			left: '10%',
-			width: '70%',
-			color: 'white'
+		thumbstyle: {
+			width: '800px',
+			height: '320px'
 		},
-		titlerows:'3',
-		titlestyle: {
-			top: '30%',
-			left: '10%',
-			width: '70%',
-			color: 'white'
-		},
-		bodystyle: {
-			maxWidth: '40%',
-			bottom: '10%',
-			left: '0',
-			transform: 'translateY(-50%)',
-			backgroundColor: 'white'
-		}
-	},
-	{
-		imgsrc: "/images/banners/banner09.png",
-		name: "LinkedIn",
 		show: true,
 		logostyle: {
 			top: '10%',
@@ -218,11 +264,12 @@ var LAZYOBJ = [
 			width: '15%',
 			color: 'white'
 		},
-		titlerows: '2',
+		titlerows: '3',
 		titlestyle: {
-			top: '20%',
+			top: '30%',
 			left: '4%',
-			width: '50%',
+			maxWidth: '40%',
+			fontSize: '32px',
 			color: 'white'
 		},
 		bodystyle: {
@@ -232,22 +279,38 @@ var LAZYOBJ = [
 		}
 	},
 	{
-		imgsrc: "/images/banners/banner10.png",
+		imgsrc: "/images/banners/320x50.png",
 		name: "Mobile",
+		thumbstyle: {
+			width: '320px',
+			height: '50px'
+		},
 		show: true,
 		logostyle: {
 			top: '50%',
-			left: '50%',
-			width: '30%',
-			transform: 'translateY(-50%) translateX(-50%)',
+			left: '5%',
+			width: '20%',
+			transform: 'translateY(-50%)',
 			color: 'white'
 		},
-		titlerows: '2',
+		titlerows: '1',
 		titlestyle: {
-			display: 'none'
+			top: '50%',
+			left: '50%',
+			width: '100%',
+			textAlign: 'center',
+			color: 'white',
+			fontSize: '16px',
+			maxWidth: '40%',
+			transform: 'translateY(-50%) translateX(-50%)',
 		},
 		bodystyle: {
-			display: 'none'
+			top: '50%',
+			right: '4%',
+			color: 'white',
+			fontSize: '6px',
+			maxWidth: '15%',
+			transform: 'translateY(-50%)'
 		}
 	}
 ]
--- public/scripts/react.js
+++ public/scripts/react.js
@@ -1,3 +1,5 @@
+var Draggable = ReactDraggable;
+
 var Lightbox = React.createClass({
 	handleClose: function() {
 		this.props.onLightboxClose();
@@ -14,7 +16,11 @@ var Lightbox = React.createClass({
 		var index = this.props.imgindex;
 		var imgsrc = this.props.metadata[index].imgsrc;
 		var imgdata = this.props.metadata[index];
+		var imgstyle = this.props.metadata[index].thumbstyle;
 		var lightboxval = true;
+		if (this.props.imgsrc !== '') {
+			imgsrc = this.props.imgsrc;
+		};
 		if (this.props.lightbox) {
 			return (
 				<div className="lightbox">
@@ -22,8 +28,8 @@ var Lightbox = React.createClass({
 						<div onClick={this.getPrevImg} className="lightbox__arrow lightbox__arrow--prev">
 							<img src="images/arrow.png" />
 						</div>
-						<div className="lightbox__image">
-							<img src={ imgsrc } />
+						<div className="lightbox__image" style={this.imgstyle} >
+							<img src={ imgsrc } style={imgstyle} />
 							<BannerContent onCopyChange={this.props.onCopyChange} lightbox={lightboxval} titletxt={this.props.titletxt} bodytxt={this.props.bodytxt} imgdata={imgdata} />
 						</div>
 						<div onClick={this.getNextImg} className="lightbox__arrow">
@@ -63,17 +69,9 @@ var BannerContent = React.createClass({
 		this.props.onCopyChange(this.state.title);
 	},
 	render: function() {
-		var lightboxfont;
-		var logosrc;
-		if (this.props.lightbox) {
-			lightboxfont = { fontSize: '40px' }
-		} else {
-			lightboxfont = { fontSize: '10px' }
-		}
-		
-		logosrc = "/images/eblogo-" + this.props.imgdata.logostyle.color + ".png";
+		var logosrc = "/images/eblogo-" + this.props.imgdata.logostyle.color + ".png";
 		return (
-			<div style={lightboxfont} > 
+			<div> 
 				<div className="thumb__logo" style={this.props.imgdata.logostyle} >
 					<img src={logosrc} />
 				</div>
@@ -90,14 +88,33 @@ var BannerThumb = React.createClass({
 	handleClick: function() {
 		this.props.onImgChange(this.props.imgid);
 	},
+	handleStart: function (event, ui) {
+        console.log('Event: ', event);
+        console.log('Position: ', ui.position);
+    },
+
+    handleDrag: function (event, ui) {
+        console.log('Event: ', event);
+    console.log('Position: ', ui.position);
+    },
+
+    handleStop: function (event, ui) {
+        console.log('Event: ', event);
+    console.log('Position: ', ui.position);
+    },
 	render: function() {
 		var thumbClick = this.handleClick;
 		var lightboxval = false;
 		var activeStyle = {};
+		var imgsrc = this.props.imgdata.imgsrc;
+		var test_false = false;
 		if (!this.props.imgdata.show) {
 			activeStyle = {
 				display: 'none'
 			}
+		}
+		if (this.props.imgsrc !== '') {
+			imgsrc = this.props.imgsrc
 		}
 		return ( 
 			<div style={activeStyle}>
@@ -108,9 +125,21 @@ var BannerThumb = React.createClass({
 					<div className="thumb__line">
 					</div>
 				</div>
-				<div onClick={thumbClick} className="thumb"> 
+				<div className="thumb" style={this.props.imgdata.thumbstyle} > 
 
-					<img src={this.props.imgdata.imgsrc} />
+					<Draggable 
+		                handle=".handle"
+		      			bounds={{right: 0, bottom: 0}}
+		                onStart={this.handleStart}
+		                onDrag={this.handleDrag}
+		                onStop={this.handleStop}>
+
+		                <div className="thumb__drag">
+		               		<img draggable={test_false} className="handle" id={this.props.imgdata.name} src={imgsrc} />
+		               	</div>
+
+		            </Draggable>
+
 					<BannerContent onCopyChange={this.props.onCopyChange} lightbox={lightboxval} titletxt={this.props.titletxt} bodytxt={this.props.bodytxt} imgdata={this.props.imgdata} />
 				</div>
 			</div>
@@ -126,12 +155,20 @@ var BannerCollection = React.createClass({
 		var self = this;
 		var thumbNodes = this.props.metadata.map(function(item, index) {
 			return (
-				<BannerThumb onCopyChange={self.props.onCopyChange} onImgChange={self.handleImgChange} titletxt={self.props.titletxt} bodytxt={self.props.bodytxt} imgid={index} imgdata={item} />
+				<BannerThumb imgsrc={self.props.imgsrc} onCopyChange={self.props.onCopyChange} onImgChange={self.handleImgChange} titletxt={self.props.titletxt} bodytxt={self.props.bodytxt} imgid={index} imgdata={item} />
 			)
+		});
+		var gradNodes = this.props.metadata.map(function(item, index) {
+			if (item.name !== "FacebookContext") {
+				return (
+					<BannerThumb imgsrc={self.props.gradsrc} onCopyChange={self.props.onCopyChange} onImgChange={self.handleImgChange} titletxt={self.props.titletxt} bodytxt={self.props.bodytxt} imgid={index} imgdata={item} />
+				)
+			}
 		});
 		return (
 			<div id="main" className="collection">
 				{thumbNodes}
+				{gradNodes}
 			</div>
 		)
 	}
@@ -157,6 +194,25 @@ var DimField = React.createClass({
 			)
 		}
 	}
+});
+
+var GradItem = React.createClass({
+	handleClick: function() {
+		this.props.onGradChange(this.props.color)
+	},
+	render: function() {
+		var gradClass = "edit__gradient__item";
+		if (this.props.activeGrad === this.props.color) {
+			gradClass = "edit__gradient__item edit__gradient__item--active";
+		}
+		var gradColor = "gradient__item gradient--" + this.props.color;
+		return (
+			<div onClick={this.handleClick} className={gradClass} >
+				<div className={gradColor}>
+				</div>
+			</div>
+		)
+	}
 })
 
 var BannerEdit = React.createClass({
@@ -164,7 +220,8 @@ var BannerEdit = React.createClass({
 		return {
 			title: '',
 			body: '',
-			showEdit: "edit"
+			showEdit: "edit",
+			imgsrc: ''
 		}
 	},
 	componentDidReceiveProps: function() {
@@ -188,6 +245,13 @@ var BannerEdit = React.createClass({
 	handleCopyChange: function() {
 		this.props.onCopyChange(this.state.title, this.state.body);
 	},
+	handleNewImg: function(e) {
+		this.setState({ imgsrc: e.target.value },
+			function() {
+				this.props.onNewImg(this.state.imgsrc)
+			}
+		);
+	},
 	handleHide: function() {
 		if (this.state.showEdit === "edit") {
 			this.setState({ showEdit: "edit edit--hide"} );
@@ -204,18 +268,29 @@ var BannerEdit = React.createClass({
 		});
 		return (
 			<div className={this.state.showEdit} > 
-				<div onClick={this.handleHide} className="edit__item edit__title">
+				<div onClick={this.handleHide} id="foo" className="edit__item edit__title">
 					Edit Copy
 				</div>
 				<div className="edit__line">
 				</div>
 				<form>
+					<span>Title:</span> 
 					<input className="edit__item edit__area" placeholder="edit title text" type="text" value={this.props.titletxt} onChange={this.handleTitleChange} /> 
+					<span>Button:</span> 
 					<input className="edit__item edit__area" placeholder="edit button text" type="text" value={this.props.bodytxt} onChange={this.handleBodyChange} /> 
-					<input className="edit__item edit__area" placeholder="image source" type="text" />
+					<span>Image Source:</span> 
+					<input className="edit__item edit__area" placeholder="image source" type="text" value={this.props.imgsrc} onChange={this.handleNewImg} />
 				</form>
+				<div className="edit__gradient">
+					<GradItem onGradChange={this.props.onGradChange} activeGrad={this.props.activeGrad} color="green" />
+					<GradItem onGradChange={this.props.onGradChange} activeGrad={this.props.activeGrad} color="blue" />
+					<GradItem onGradChange={this.props.onGradChange} activeGrad={this.props.activeGrad} color="red" />
+				</div>
 				<div className="edit__dims">
 					{ dimNodes }
+				</div>
+				<div className="edit__export">
+					Export as JPEG
 				</div>
 			</div>
 		)
@@ -229,8 +304,20 @@ var BannerApp = React.createClass({
 			bodytxt: '',
 			imgindex: 0,
 			lightbox: false,
-			metadata: this.props.metadata
+			metadata: this.props.metadata,
+			imgsrc: '',
+			activeGrad: 'green',
+			gradsrc: 'images/banners/green.png'
 		}
+	},
+	handleGradChange: function(color) {
+		var gradsrc = "images/banners/"+color+".png";
+		this.setState({ activeGrad: color });
+		this.setState({ gradsrc: gradsrc });
+		console.log(gradsrc);
+	},
+	handleNewImg: function(imgsrc) {
+		this.setState({ imgsrc: imgsrc })
 	},
 	handleImgChange: function(imgindex) {
 		this.setState({ imgindex: imgindex });
@@ -251,16 +338,19 @@ var BannerApp = React.createClass({
 		meta[dimIndex].show = !show;
 		this.setState({	metadata: meta });
 	},
+	
 	render: function() {
 		return (
 			<div>
 				<div className="container">
-					<BannerCollection onCopyChange={this.handleCopyChange} onImgChange={this.handleImgChange} titletxt={this.state.titletxt} bodytxt={this.state.bodytxt} metadata={this.state.metadata} />
-					<BannerEdit onDimClick={this.handleDimClick} onCopyChange={this.handleCopyChange} titletxt={this.state.titletxt} bodytxt={this.state.bodytxt} metadata={this.state.metadata} />
+					<BannerCollection gradsrc={this.state.gradsrc} imgsrc={this.state.imgsrc} onCopyChange={this.handleCopyChange} onImgChange={this.handleImgChange} titletxt={this.state.titletxt} bodytxt={this.state.bodytxt} metadata={this.state.metadata} />
+					<BannerEdit onGradChange={this.handleGradChange} activeGrad={this.state.activeGrad} imgsrc={this.state.imgsrc} onNewImg={this.handleNewImg} onDimClick={this.handleDimClick} onCopyChange={this.handleCopyChange} titletxt={this.state.titletxt} bodytxt={this.state.bodytxt} metadata={this.state.metadata} />
 				</div>
-				<Lightbox onCopyChange={this.handleCopyChange} onImgChange={this.handleImgChange} titletxt={this.state.titletxt} bodytxt={this.state.bodytxt} metadata={this.state.metadata} imgindex={this.state.imgindex} onLightboxClose={this.handleLightboxClose} lightbox={this.state.lightbox} />
+				
+				{/*<Lightbox imgsrc={this.state.imgsrc} onCopyChange={this.handleCopyChange} onImgChange={this.handleImgChange} titletxt={this.state.titletxt} bodytxt={this.state.bodytxt} metadata={this.state.metadata} imgindex={this.state.imgindex} onLightboxClose={this.handleLightboxClose} lightbox={this.state.lightbox} />*/}
 			</div>
 		)
+		
 	}
 });
 
